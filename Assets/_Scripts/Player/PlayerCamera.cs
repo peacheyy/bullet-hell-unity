256395fb8f0a84bc1efe3f48b6ee1a24fe53f183
peacheyy <evanpeachey@gmail.com>
2025-01-28 19:30:12 -0500

    changed game settings to fit pixel art style, now player has visual glitch

--- Assets/_Scripts/Player/PlayerCamera.cs
+++ Assets/_Scripts/Player/PlayerCamera.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     [SerializeField] Transform target;
-    
-    void LateUpdate() {
+    [SerializeField] float pixelSize = 1f / 32f; // Adjust based on your PPU
+
+    void LateUpdate()
+    {
         Vector3 desiredPosition = new Vector3(
             target.position.x,
             target.position.y,
             transform.position.z
         );
-        
-        //lerp used here makes movement of camera more smooth
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime);
-    }   
+
+        // Snap to the nearest pixel
+        desiredPosition.x = Mathf.Round(desiredPosition.x / pixelSize) * pixelSize;
+        desiredPosition.y = Mathf.Round(desiredPosition.y / pixelSize) * pixelSize;
+
+        // Smoothly move the camera
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 10f);
+    }
 }
