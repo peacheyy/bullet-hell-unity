77fb5b3f9b89e585359a98a2226ce1e21f6f95fa
peacheyy <evanpeachey@gmail.com>
2025-02-18 18:58:12 -0500

    added health bar, weird visual glitch where bar rotates with player

--- Assets/_Scripts/Enemy/Enemy.cs
+++ Assets/_Scripts/Enemy/Enemy.cs
@@ -5,17 +5,22 @@ public class Enemy : MonoBehaviour
     [Header("Enemy Stats")]
     [SerializeField] float maxHealth = 25f;
 
+    public HealthBar healthBar;
+
     private float _currentHealth;
     public static event System.Action<Enemy> OnEnemyDeath;
 
     void Start()
     {
         _currentHealth = maxHealth;
+        healthBar.SetMaxHealth((int)maxHealth);
     }
 
     public void TakeDamage(float damage)
     {
         _currentHealth -= damage;
+        
+        healthBar.SetHealth((int)_currentHealth);
 
         if (_currentHealth <= 0)
         {
--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -5,16 +5,21 @@ public class Player : MonoBehaviour
     [Header("Player Stats")]
     [SerializeField] float maxHealth = 100f;
 
+    public HealthBar healthBar;
+
     private float _currentHealth;
 
     void Start()
     {
         _currentHealth = maxHealth;
+        healthBar.SetMaxHealth((int)maxHealth);
     }
 
     public void TakeDamage(float damage)
     {
         _currentHealth -= damage;
+
+        healthBar.SetHealth((int)_currentHealth);
 
         if (_currentHealth <= 0)
         {
